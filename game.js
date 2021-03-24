a0ed9fef2f618484ddb2dff91455320fc71f2155
Alex Henderson <bohroco@gmail.com>
2021-03-24 16:36:50 -0500

    Update game.js
    Add upgrade to 1ncrease the growth of your number.

--- game.js
+++ game.js
@@ -1,5 +1,21 @@
+const nD = (num) => { return new Decimal(number) }
+const $ = (id) => { return document.getElementById(id) }
+
 const initiate = () => {
+  load();
+  setInterval(progress(), 50);
+}
+
+const progress = () => {
+ const diff = new Date().getTime() - player.lastTick;
+ player.number = player.number.plus(diff.div(1000).times(player.upgrade[0]);
 }
 
 const increaseNumber = () => {
+  player.upgrade[0] = player.upgrade[0].plus(1);
+}
+
+const load = () => {
+ if(localStorage.getItem("firstGameSave") != null) player = localStorage.getItem("firstGameSave");
+ return player;
 }
