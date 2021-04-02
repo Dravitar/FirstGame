d285ffb966d761843ffcd1e0db6076de3f90706d
Alex Henderson <bohroco@gmail.com>
2021-04-02 13:43:52 -0500

    Added toggle variables for the automators

--- player.js
+++ player.js
@@ -8,6 +8,7 @@ const getDefaultPlayer = () => {
    autoUpgradeCycle: [0, 0,],
    autoUpgradePrice: [nD(25),nD(100),],
    autoUpgradePriceIncrease: [nD(1.1),nD(2),],
+   autoUpgradeActive: [true,true,],
    upgrade: [0,0,0],
    upgradePrice: [nD(10),nD(100),nD(1500),],
    upgradePriceIncrease: [nD(100),nD(10),nD(10)],
