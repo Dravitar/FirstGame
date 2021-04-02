67d973f50d0014dcf51f820eab21a12d119a2635
Alex Henderson <bohroco@gmail.com>
2021-04-02 11:32:19 -0500

    Transfer player.js from glitch to GitHub

--- player.js
+++ player.js
@@ -1,9 +1,21 @@
+const nD = (num) => { return new Decimal(num) }
+
 const getDefaultPlayer = () => {
  return {
    number: nD(0),
-   upgrade: [0,],
+   maxNum: nD(0),
+   autoUpgrade: [nD(0),nD(0),],
+   autoUpgradeCycle: [0, 0,],
+   autoUpgradePrice: [nD(25),nD(100),],
+   autoUpgradePriceIncrease: [nD(1.1),nD(2),],
+   upgrade: [0,0,0],
+   upgradePrice: [nD(10),nD(100),nD(1500),],
+   upgradePriceIncrease: [nD(100),nD(10),nD(10)],
+   oneness: nD(0),
+   maxOneness: nD(0),
+   formatVersion: 1,
    lastTick: new Date().getTime(),
  }
 }
 
-const player = getDefaultPlayer();
+var player = getDefaultPlayer();
